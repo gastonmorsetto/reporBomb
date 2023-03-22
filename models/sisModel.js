4f4629ffe6b5eb5b113c8d9dcf2de4702faddb0d
Gaston Morsetto <gastonmorsetto@gmail.com>
2023-03-22 18:22:40 -0300

    Eliminacion de carpeta reports

--- models/sisModel.js
+++ models/sisModel.js
@@ -1,9 +1,10 @@
 module.exports = {
     obtener: function(conexion, funcion) {
-        conexion.query('SELECT * FROM datos', funcion);
+        conexion.query('SELECT id,date_format(fecha,"%d/%m/%Y - %H:%i %p") as fecha,tipo,zona FROM datos', funcion);
     },
 
     insertar: function(conexion, datos, funcion) {
-        conexion.query('INSERT INTO datos (tipo, zona) VALUES (?,?)', [datos.nombre, datos.tipo], funcion);
+        var myDate = new Date();
+        conexion.query('INSERT INTO datos (fecha,tipo, zona) VALUES (?,?,?)', [myDate, datos.nombre, datos.tipo], funcion);
     }
 }
