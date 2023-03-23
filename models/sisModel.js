b21fd20d1944bd65c2b2d7b7c55eeba48a36f475
Gaston Morsetto <gastonmorsetto@gmail.com>
2023-03-23 00:13:03 -0300

    Se agrego funcionalidad de despligue de opciones en base a radiobutton y guardado en DB de esa eleccion

--- models/sisModel.js
+++ models/sisModel.js
@@ -1,10 +1,10 @@
 module.exports = {
     obtener: function(conexion, funcion) {
-        conexion.query('SELECT id,date_format(fecha,"%d/%m/%Y - %H:%i %p") as fecha,tipo,zona FROM datos', funcion);
+        conexion.query('SELECT id,date_format(fecha,"%d/%m/%Y - %H:%i %p") as fecha,tipo,zona,siniestro FROM datos', funcion);
     },
 
     insertar: function(conexion, datos, funcion) {
         var myDate = new Date();
-        conexion.query('INSERT INTO datos (fecha,tipo, zona) VALUES (?,?,?)', [myDate, datos.nombre, datos.tipo], funcion);
+        conexion.query('INSERT INTO datos (fecha,tipo,zona,siniestro) VALUES (?,?,?,?)', [myDate, datos.nombre, datos.zona,datos.inInfo], funcion);
     }
 }
